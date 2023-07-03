6634d6ce2dde790672333f1f64d8acaf7c45c904
manujagobind <manujagobind@gmail.com>
2023-07-03 23:06:54 +0530

    Display home page on /

--- brand/views.py
+++ brand/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 from brand.models import Brand
 
 
+def home_page(request):
+    return render(request, "index.html")
+
+
 def brand_listing(request):
     brands = Brand.objects.filter()
     return render(request, "brand-listing.html", {"brands": brands})
--- brandscanner/urls.py
+++ brandscanner/urls.py
@@ -17,10 +17,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path("", TemplateView.as_view(template_name="index.html")),
     path("brands/", include("brand.urls", namespace="brand")),
-    path("admin/", admin.site.urls),
+    path("dont_be_evil/", admin.site.urls),
 ]
 
 if settings.DEBUG:
