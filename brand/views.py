d3a4dcdc148a5841ef3a9e147d56e9706d65635c
manujagobind <manujagobind@gmail.com>
2023-07-04 22:47:04 +0530

    Filter values to be dynamic

--- brand/views.py
+++ brand/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
-from brand.models import Brand
+from brand.models import Brand, Category, Tag
+
+BRAND_TAGS = {
+    "location": Brand.Location.choices,
+    "indicative_pricing": Brand.IndicativePricing.choices,
+}
 
 
 def home_page(request):
@@ -8,5 +13,25 @@ def home_page(request):
 
 
 def brand_listing(request):
-    brands = Brand.objects.filter()
-    return render(request, "brand-listing.html", {"brands": brands})
+    selected_category = "Footwear"
+
+    category = Category.objects.get(name=selected_category)
+
+    brands = Brand.objects.filter(brandcategory__category__name=category.name)
+    tags = Tag.objects.filter(category__name=category.name)
+
+    category_tags = dict()
+    for tag in tags:
+        if tag.name not in category_tags:
+            category_tags[tag.name] = list()
+        category_tags[tag.name].append(tag.value)
+
+    return render(
+        request,
+        "brand-listing.html",
+        {
+            "brands": brands,
+            "category_tags": category_tags,
+            "brand_tags": BRAND_TAGS,
+        },
+    )
