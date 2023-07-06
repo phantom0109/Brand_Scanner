9de06a1f70389ed9d735d876936e58251c7764fb
manujagobind <manujagobind@gmail.com>
2023-07-06 22:11:54 +0530

    Filter brands based on tags in query params

--- brand/views.py
+++ brand/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.db.models import Count, Q
+from django.shortcuts import get_object_or_404, render
 
 from brand.models import Brand, Category, Tag
 
@@ -13,18 +14,45 @@ def home_page(request):
 
 
 def brand_listing(request, selected_category):
-    category = Category.objects.get(internal_name=selected_category)
+    category = get_object_or_404(Category, internal_name=selected_category)
 
-    brands = Brand.objects.filter(
-        brandcategory__category__internal_name=category.internal_name
-    )
     tags = Tag.objects.filter(category__internal_name=category.internal_name)
+    tag_names = set(tags.values_list("name", flat=True))
 
+    # For filter options
     category_tags = dict()
     for tag in tags:
         if tag.name not in category_tags:
             category_tags[tag.name] = list()
         category_tags[tag.name].append(tag.value)
+
+    # Get tags to filter brands based on query params
+    query_filter = None
+    for tag_name in tag_names:
+        tag_values = request.GET.getlist(tag_name)
+
+        for tag_value in tag_values:
+            if query_filter is None:
+                query_filter = Q(name=tag_name, value=tag_value)
+            else:
+                query_filter |= Q(name=tag_name, value=tag_value)
+
+    if query_filter is not None:
+        tags = tags.filter(query_filter)
+
+    brands = None
+    if query_filter is not None:
+        # Filter brands
+        brands = (
+            Brand.objects.filter(brandtag__tag__in=tags)
+            .annotate(match_count=Count("brandtag"))
+            .filter(match_count=tags.count())
+        )
+    else:
+        # Return all brands if no filter is applied
+        brands = Brand.objects.filter(
+            brandcategory__category__internal_name=category.internal_name
+        )
 
     return render(
         request,
