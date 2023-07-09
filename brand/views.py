45ff3f18ee0ac35be47a5fec54e6c1586d8aa2d3
manujagobind <manujagobind@gmail.com>
2023-07-09 20:53:37 +0530

    Filter on brand tags and pre-fil display selected tags

--- brand/views.py
+++ brand/views.py
@@ -1,3 +1,5 @@
+import copy
+
 from django.core.paginator import Paginator
 from django.db.models import Count, Q
 from django.shortcuts import get_object_or_404, render
@@ -6,9 +8,23 @@ from brand.models import Brand, Category, Tag
 
 PAGE_SIZE = 20
 BRAND_TAGS = {
-    "location": Brand.Location.choices,
-    "pricing": Brand.IndicativePricing.choices,
+    # Keys are model field names
+    "location": [
+        {"value": item[0], "display_name": item[1]}
+        for item in Brand.Location.choices
+    ],
+    "indicative_pricing": [
+        {"value": item[0], "display_name": item[1]}
+        for item in Brand.IndicativePricing.choices
+    ],
 }
+
+
+def _update_display_tags(category_tags, tag_name, tag_value, update_fields):
+    for option in category_tags[tag_name]:
+        if option["value"] == tag_value:
+            for field in update_fields:
+                option[field] = update_fields[field]
 
 
 def home_page(request):
@@ -19,18 +35,18 @@ def brand_listing(request, selected_category):
     category = get_object_or_404(Category, internal_name=selected_category)
 
     tags = Tag.objects.filter(category__internal_name=category.internal_name)
-    tag_names = set(tags.values_list("name", flat=True))
+    category_tag_names = set(tags.values_list("name", flat=True))
 
     # For filter options
     category_tags = dict()
     for tag in tags:
         if tag.name not in category_tags:
             category_tags[tag.name] = list()
-        category_tags[tag.name].append(tag.value)
+        category_tags[tag.name].append({"value": tag.value})
 
-    # Get tags to filter brands based on query params
+    # Filter brands based on category tags provided in request params
     query_filter = None
-    for tag_name in tag_names:
+    for tag_name in category_tag_names:
         tag_values = request.GET.getlist(tag_name)
 
         for tag_value in tag_values:
@@ -38,6 +54,10 @@ def brand_listing(request, selected_category):
                 query_filter = Q(name=tag_name, value=tag_value)
             else:
                 query_filter |= Q(name=tag_name, value=tag_value)
+
+            _update_display_tags(
+                category_tags, tag_name, tag_value, {"selected": True}
+            )
 
     if query_filter is not None:
         tags = tags.filter(query_filter)
@@ -56,10 +76,31 @@ def brand_listing(request, selected_category):
             brandcategory__category__internal_name=category.internal_name
         )
 
+    # Further filter brands based on brand tags provided in request params
+    brand_tags = copy.deepcopy(BRAND_TAGS)
+    brand_tag_names = brand_tags.keys()
+    for tag_name in brand_tag_names:
+        tag_values = request.GET.getlist(tag_name)
+
+        query_filter = None
+        for tag_value in tag_values:
+            if query_filter is None:
+                query_filter = Q(**{f"{tag_name}": tag_value})
+            else:
+                query_filter |= Q(**{f"{tag_name}": tag_value})
+
+            _update_display_tags(
+                brand_tags, tag_name, tag_value, {"selected": True}
+            )
+
+        if query_filter is not None:
+            brands = brands.filter(query_filter)
+
     paginator = Paginator(brands, PAGE_SIZE)
     page_number = request.GET.get("page", 0)
     page = paginator.get_page(page_number)
 
+    print(brand_tags)
     return render(
         request,
         "brand-listing.html",
@@ -68,7 +109,7 @@ def brand_listing(request, selected_category):
             "brands": page,
             "total_brands": len(brands),
             "category_tags": category_tags,
-            "brand_tags": BRAND_TAGS,
+            "brand_tags": brand_tags,
         },
     )
 
