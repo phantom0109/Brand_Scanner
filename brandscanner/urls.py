b734216ecbcd7c1dac4bb194c7d0e3f7691c773a
PHANTOM <92105329+phantom0109@users.noreply.github.com>
2024-07-29 14:52:55 +0800

    Update urls.py

--- brandscanner/urls.py
+++ brandscanner/urls.py
@@ -6,6 +6,7 @@ Examples:
 Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    3. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
